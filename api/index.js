2370bb46fe39a9f31419e18b0b06136ffaf75b5f
Prath-999 <prathmeshabhang75@gmail.com>
2024-11-18 05:38:28 -0800

    Connected MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+.then(
+    ()=>{console.log('DB Connected')
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 const app=express()
 
 app.listen(3000, ()=>{
-    console.log("Running")
+    console.log("Server is Running")
 })
