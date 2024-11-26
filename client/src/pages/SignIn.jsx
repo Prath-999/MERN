db4ff01219e9a9835ad78526fd24d4cb7c4e5a83
Prath-999 <prathmeshabhang75@gmail.com>
2024-11-26 07:43:00 -0800

    Completed signin functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignIn() {
+  const [formdata, setFormData]=useState({});
+  const [errorMessage, setErrorMessage]=useState(null);
+  const [loading, setLoading]=useState(false);
+  const navigate=useNavigate()
+  const handleChange=(e)=>{
+    setFormData({...formdata, [e.target.id]:e.target.value.trim()})
+  }
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    if(!formdata.email || !formdata.password){
+      return setErrorMessage("Please fill out all fields!")
+    }
+    try{
+      setLoading(true);
+      setErrorMessage(null);
+      const res=await fetch('/api/auth/signin',{
+        method:'POST',
+        headers:{'Content-type':'application/json'},
+        body:JSON.stringify(formdata),
+      })
+      const data=await res.json();
+      if(data.success === false){
+        setLoading(false)
+        return setErrorMessage(data.message)
+      }
+      setLoading(false)
+      if(res.ok){
+        navigate('/');
+      }
+    } catch(error){
+      setErrorMessage(error.message)
+      setLoading(false)
+    }
+  }
   return (
-    <div>
-      SignIn
+    <div className='min-h-screen mt-50'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/*left side of signIn page*/}
+        <div className='flex-1'>
+          <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-600
+              rounded-lg text-white'>Campus
+            </span>
+            Connect
+          </Link>
+          <p className='text-sm mt-5'>Hello. Welcome to CampusConnect!!!</p>
+          <p className='text-sm'>You can sign in with your email and password.</p>
+        </div>
+        {/*right side of signIn page*/}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            
+            <div>
+              <Label className='font-bold' value='Your email'></Label>
+              <TextInput type='email'
+              placeholder='name@company.com'
+              id='email'
+              onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label className='font-bold' value='Your password'></Label>
+              <TextInput type='password'
+              placeholder='**********'
+              id='password'
+              onChange={handleChange}
+              />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm'></Spinner>
+                    <span className='pl-3'>Loading...</span>
+                  </>
+                ) : 'Sign In'
+              }
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>
+              Don't have an account?
+            </span>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign Up
+            </Link>
+          </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
     </div>
   )
 }
